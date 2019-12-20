b7a8c0a49e336cd67d54642b387cda33a6ef002d
Vincent <crispalav98@gmail.com>
2019-12-20 12:24:25 +0100

    fix sintassi import

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-from flask import Flask, render-template
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     scritta="Ciao mondo!"
-    return render_template("base.html", testo=scritta ) 
+    return render_template("base.html", testo=scritta )
+
 @app.route('/info')
 def info():
     scritta="informazioni"
     return render_template("base.html", testo=scritta ) 
-
 
 if __name__ == '__main__':
     app.run()
